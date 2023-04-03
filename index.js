ad7992a2e3c3603eb96da5a09e53a1b714f455b2
Youssef-ML <114027612+youssefML7@users.noreply.github.com>
2023-04-03 00:05:45 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var emailME = document.getElementById("emailme")
+var sir = document.getElementById("sir")
+email = "Theprofessionalcoder@outlook.fr"
+
+function show() {
+	emailME.innerHTML = email
+	console.log("emailme")
+}
+
